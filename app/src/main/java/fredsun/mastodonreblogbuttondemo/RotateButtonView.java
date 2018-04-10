e53839ecb748dfa0c28c75655e134118d35be406
sunxl <fredsun1206@gmail.com>
2018-04-10 23:51:43 +0800

    feat: 删除不必要的变量, 补充注释

--- app/src/main/java/fredsun/mastodonreblogbuttondemo/RotateButtonView.java
+++ app/src/main/java/fredsun/mastodonreblogbuttondemo/RotateButtonView.java
@@ -26,8 +26,6 @@ public class RotateButtonView extends View {
     Paint paint, paintTriangle, paintTrans;
     private float[] pos = new float[2];
     private float[] tan = new float[2];
-    private RectF mRectF;
-    private int mCenterX, mCenterY;
     private int mWidth, mHeight;
     float mAnimatorValue;
     float rectWidth, rectHeight;
@@ -51,7 +49,6 @@ public class RotateButtonView extends View {
         paintTriangle = new Paint();
         paintTrans = new Paint();
         mPathMeasure = new PathMeasure();
-        mRectF = new RectF();
 
         Drawable background = getBackground();
         if (background instanceof ColorDrawable){
@@ -74,10 +71,6 @@ public class RotateButtonView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         mWidth = w;
         mHeight = h;
-        mCenterX = w/2;
-        mCenterY = h/2;
-        pos[0] = -w/4;
-        pos[1] = 0;
         rectWidth = mWidth * 12 / 42;
         rectHeight =  mHeight* 9 / 36;
         triangleWidth = mWidth * 18 / 42;
@@ -109,6 +102,7 @@ public class RotateButtonView extends View {
 //        path.arcTo(rectLeftBottom, 90, 90, false);
 //        path.lineTo(-rectWidth, -offset);
 
+//        //绘制圆角矩形
         path.moveTo(-rectWidth, -offset);//左侧中间点
         path.lineTo(-rectWidth, -(rectHeight-strokeWidth));
         RectF rectF = new RectF(-rectWidth, -rectHeight, -(rectWidth - 2 * strokeWidth/2), -(rectHeight - 2 * strokeWidth/2));
@@ -131,6 +125,7 @@ public class RotateButtonView extends View {
         paint.setStrokeWidth(strokeWidth);
         paint.setColor(getResources().getColor(R.color.colorAccent));
 
+//        //绘制左侧背景色条
         pathTrans.moveTo(offset,-triangleWidth/2);
         pathTrans.lineTo(triangleHeight+offset,0);
         pathTrans.lineTo(offset,triangleWidth/2);
@@ -139,8 +134,9 @@ public class RotateButtonView extends View {
         xfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_OVER);
         paintTrans.setXfermode(xfermode);
 
+//        //绘制左侧三角形
         paintTriangle.setStyle(Paint.Style.FILL);
-        CornerPathEffect cornerPathEffect = new CornerPathEffect(50);
+        CornerPathEffect cornerPathEffect = new CornerPathEffect(50);//圆角
         paintTriangle.setPathEffect(cornerPathEffect);
         paintTriangle.setColor(getResources().getColor(R.color.colorAccent));
         pathTriangle.lineTo(0,-triangleWidth / 2);
@@ -148,11 +144,13 @@ public class RotateButtonView extends View {
         pathTriangle.lineTo(0,triangleWidth / 2);
         pathTriangle.close();
 
+        //绘制右侧三角形
         pathTriangleRight.lineTo(0,-triangleWidth/2);
         pathTriangleRight.lineTo(-triangleHeight,0);
         pathTriangleRight.lineTo(0,triangleWidth/2);
         pathTriangleRight.close();
 
+        //绘制右侧背景色条
         pathTransRight.moveTo(-offset,-triangleWidth/2);
         pathTransRight.lineTo(-triangleHeight-offset,0);
         pathTransRight.lineTo(-offset,triangleWidth/2);
@@ -173,39 +171,30 @@ public class RotateButtonView extends View {
         canvas.drawPath(path, paint);
 
         float degree = (float) (Math.atan2(tan[1], tan[0])*180.0/ Math.PI);
-        Log.i("LineView","degree"+degree);
+        //坐标系移动到左侧的path起点
         canvas.translate(pos[0],pos[1]);
+        //画布旋转趋势与x轴的夹角
         canvas.rotate(degree);
-//        canvas.drawLine(0,-200, 200, 0, paintTriangle);
-//        canvas.drawLine(200,0, 0, 200, paintTriangle);
-//        canvas.drawLine(0,200, 0, -200, paintTriangle);
         int i = canvas.saveLayer(null, null, Canvas.ALL_SAVE_FLAG);
         canvas.drawPath(pathTriangle, paintTriangle);
         canvas.drawPath(pathTrans, paintTrans);
         canvas.restoreToCount(i);
-
+        //画布画布转回原来的夹角
         canvas.rotate(-degree);
+        //坐标系移动到原点
         canvas.translate(-pos[0], -pos[1]);
+        //坐标系移动到右侧的path起点
         canvas.translate(-pos[0], -pos[1]);
+        //画布旋转趋势与x轴的夹角
         canvas.rotate(degree);
-
         int j = canvas.saveLayer(null, null, Canvas.ALL_SAVE_FLAG);
         canvas.drawPath(pathTriangleRight, paintTriangle);
         canvas.drawPath(pathTransRight, paintTrans);
         canvas.restoreToCount(j);
-
         canvas.restore();
-        Log.i("LineView","pos[0]+"+pos[0]);
-        Log.i("LineView","pos[1]+"+pos[1]);
-        Log.i("LineView","tan[0]+"+tan[0]);
-        Log.i("LineView","tan[1]+"+tan[1]);
-        Log.i("LineView","mAnimatorValue+"+mAnimatorValue);
-
-
     }
 
     void startMove(){
-
         //创建一个值从0到xxx的动画
         ValueAnimator valueAnimator = ValueAnimator.ofFloat(0,1);
         valueAnimator.setInterpolator(new DecelerateInterpolator());
@@ -215,9 +204,6 @@ public class RotateButtonView extends View {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 mAnimatorValue = (float) animation.getAnimatedValue();
-                Log.i("LineView","mAnimatorValue+"+mAnimatorValue);
-                Log.i("LineView","distance+"+mAnimatorValue * mPathMeasure.getLength());
-
                 postInvalidate();
             }
         });
